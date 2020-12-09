a50dfc180e3665f02b45bfe0d0d8c88d576e20a0
Mark <git@happybeing.com>
2020-12-09 18:11:48 +0000

    Add Issues support, New repo and unify with Clone

--- src/repo/repo.go
+++ src/repo/repo.go
@@ -6,17 +6,20 @@ package repo // TODO maybe rename to 'api' or something not 'repo'
 import (
 	"errors"
 	"fmt"
+	"path/filepath"
 	"strings"
 	"syscall/js"
 
 	// "github.com/go-git/go-git/v5"
 
 	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5/plumbing/cache"
 	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/go-git/go-git/v5/storage/memory"
 
+	bugcache "github.com/MichaelMure/git-bug/cache"
 	// "github.com/go-git/go-git/storage/memory"
 	gogit "github.com/go-git/go-git/v5"
+	storagefs "github.com/go-git/go-git/v5/storage/filesystem"
 )
 
 // Entry holds metadata and for a gogit.Repository
@@ -25,6 +28,7 @@ type Entry struct {
 	Path      string
 	URL       string
 	GogitRepo *gogit.Repository
+	GitbugRC  *bugcache.RepoCache
 }
 
 var Filesystem = memfs.New()
@@ -35,17 +39,17 @@ func GetRepositoryList(this js.Value, args []js.Value) (interface{}, error) {
 
 	repoIndex := 0
 	for path, entry := range AllRepositories {
-		cfg, err := entry.GogitRepo.Config()
-		if err != nil {
-			return nil, nil
-		}
+		// cfg, err := entry.GogitRepo.Config()
+		// if err != nil {
+		// 	return nil, nil
+		// }
 		repo := make(map[string]interface{}, 0)
 		repo["path"] = path
 		repo["host"] = entry.Host
 		repo["path"] = entry.Path
 		repo["url"] = entry.URL
-		repo["author"] = cfg.Author.Name
-		repo["author-email"] = cfg.Author.Email
+		// repo["author"] = cfg.Author.Name
+		// repo["author-email"] = cfg.Author.Email
 		retRepos[repoIndex] = repo
 		repoIndex += 1
 	}
@@ -114,94 +118,104 @@ func CloneRepository(this js.Value, args []js.Value) (interface{}, error) {
 	// println("host:", host)
 	// println("path:", path)
 	// println("proxiedURI:", proxiedURI)
+	dotGitFs, err := Filesystem.Chroot(filepath.Join(path, ".git"))
+	if err != nil {
+		return nil, err
+	}
 
-	storage := memory.NewStorage()
-	var err error
+	storage := storagefs.NewStorage(dotGitFs, cache.NewObjectLRUDefault())
 	go func() {
-		r, err := gogit.Clone(storage, Filesystem, &gogit.CloneOptions{URL: url})
+		gogitRepo, err := gogit.Clone(storage, Filesystem, &gogit.CloneOptions{URL: url})
 		if err != nil {
 			// if true {
 			println("gogit.Clone() failed: ", err.Error())
 			callback.Invoke(err.Error())
 		} else {
+			repoCache, err := OpenRepo(path)
+			if err != nil {
+				callback.Invoke(err.Error())
+			}
 			entry := Entry{
 				host,
 				path,
 				url,
-				r,
+				gogitRepo,
+				repoCache,
 			}
+			bilbo, _ := NewTestGitbugIdentity(&entry, "Bilbo Baggins", "bilbo@the.vale")
+			AddSampleBugs(&entry, bilbo)
 			AllRepositories[path] = &entry
 			callback.Invoke(js.Null())
 		}
 	}()
 
-	return nil, err
+	return nil, nil
 }
 
 // NOTE: must disable CORS in the browser using a browser plugin
 func GitCloneTest(this js.Value, args []js.Value) (interface{}, error) {
-	message := ""
-	host := ""
-	path := ""
-	url := ""
+	// message := ""
+	// host := ""
+	// path := ""
+	// url := ""
 
-	// A small test GitLab repo:
+	// // A small test GitLab repo:
 
-	// Testing ways around CORS...
+	// // Testing ways around CORS...
 
-	// gitlab.com
-	// WORKS with gitlab.com if browser CORS disabled using a plugin
-	message = "https clone of gitlab repo"
-	host = "https://gitlab.com/"
-	path = "saeedareffard1377666/testproject2.git"
+	// // gitlab.com
+	// // WORKS with gitlab.com if browser CORS disabled using a plugin
+	// message = "https clone of gitlab repo"
+	// host = "https://gitlab.com/"
+	// path = "saeedareffard1377666/testproject2.git"
 
-	// github.com
-	// WORKS using local-cors-proxy, only for a specific service:
-	// yarn global add local-cors-proxy
-	// lcp --proxyUrl https://github.com
-	host = "http://localhost:8010/proxy/"
-	path = "happybeing/p2p-git-portal-poc.git"
-	url = host + path
+	// // github.com
+	// // WORKS using local-cors-proxy, only for a specific service:
+	// // yarn global add local-cors-proxy
+	// // lcp --proxyUrl https://github.com
+	// host = "http://localhost:8010/proxy/"
+	// path = "happybeing/p2p-git-portal-poc.git"
+	// url = host + path
 
-	// WORKS with isomorphic-git proxy service:
-	// url = "https://cors.isomorphic-git.org/github.com/happybeing/p2p-git-portal-poc.git"
+	// // WORKS with isomorphic-git proxy service:
+	// // url = "https://cors.isomorphic-git.org/github.com/happybeing/p2p-git-portal-poc.git"
 
-	// FAILS without proxy (including with CORS disabled in browser)
-	// url = "https://github.com/happybeing/p2p-git-portal-poc.git"
-	// token := "<replace with PAT>"
+	// // FAILS without proxy (including with CORS disabled in browser)
+	// // url = "https://github.com/happybeing/p2p-git-portal-poc.git"
+	// // token := "<replace with PAT>"
 
-	println(message, url)
-	storage := memory.NewStorage()
-	// Note: can't wait on this or go will exit due to a deadlock (as Clone() uses http.Get())
-	go func() {
-		r, err := gogit.Clone(storage, Filesystem, &gogit.CloneOptions{
-			// Auth: &http.BasicAuth{
-			// 	Username: "noname", // This can be anything except an empty string
-			// 	Password: token,
-			// },
-			URL: url,
-		})
-		if err != nil {
-			println("gogit.Clone() failed: ", err.Error())
-		} else {
-			println("Clone complete...")
-			entry := Entry{
-				host,
-				path,
-				url,
-				r,
-			}
-			AllRepositories[path] = &entry
+	// println(message, url)
+	// storage := memory.NewStorage()
+	// // Note: can't wait on this or go will exit due to a deadlock (as Clone() uses http.Get())
+	// go func() {
+	// 	r, err := gogit.Clone(storage, Filesystem, &gogit.CloneOptions{
+	// 		// Auth: &http.BasicAuth{
+	// 		// 	Username: "noname", // This can be anything except an empty string
+	// 		// 	Password: token,
+	// 		// },
+	// 		URL: url,
+	// 	})
+	// 	if err != nil {
+	// 		println("gogit.Clone() failed: ", err.Error())
+	// 	} else {
+	// 		println("Clone complete...")
+	// 		entry := Entry{
+	// 			host,
+	// 			path,
+	// 			url,
+	// 			r,
+	// 		}
+	// 		AllRepositories[path] = &entry
 
-			// Retrieve the branch pointed by HEAD
-			ref, err2 := r.Head()
-			if err2 != nil {
-				println("r.Head() failed: ", err2.Error())
-			} else {
-				println("Retrieved head, ref: ", ref)
-			}
-		}
-	}()
+	// 		// Retrieve the branch pointed by HEAD
+	// 		ref, err2 := r.Head()
+	// 		if err2 != nil {
+	// 			println("r.Head() failed: ", err2.Error())
+	// 		} else {
+	// 			println("Retrieved head, ref: ", ref)
+	// 		}
+	// 	}
+	// }()
 
 	return nil, nil
 }
@@ -225,7 +239,6 @@ func ListHeadCommits(this js.Value, args []js.Value) (interface{}, error) {
 		return nil, nil
 	}
 	r := entry.GogitRepo
-
 	// Latest commit on current branch
 	ref, err := r.Head()
 	if err != nil {
@@ -253,6 +266,20 @@ func ListHeadCommits(this js.Value, args []js.Value) (interface{}, error) {
 	})
 
 	return nil, nil
+}
+
+// args[]:
+//	[0] path - local repo path of a cloned repository (later will be a local identifier)
+func GetIssuesForRepo(this js.Value, args []js.Value) (interface{}, error) {
+	path := args[0].String()
+	// entry, found := AllRepositories[path]
+	// if !found {
+	// 	println("Unknown repository: ", path)
+	// 	return nil, nil
+	// }
+	// r := entry.GogitRepo
+
+	return GetBugsForRepo(path)
 }
 
 // args[]:
--- src/repo/repo_gitbug.go
+++ src/repo/repo_gitbug.go
@@ -4,14 +4,19 @@
 package repo
 
 import (
+	"path/filepath"
 	"syscall/js"
 
-	"github.com/MichaelMure/git-bug/cache"
+	bugcache "github.com/MichaelMure/git-bug/cache"
+	gogit "github.com/go-git/go-git/v5"
+
 	"github.com/MichaelMure/git-bug/repository"
+	"github.com/go-git/go-git/v5/plumbing/cache"
+	storagefs "github.com/go-git/go-git/v5/storage/filesystem"
 )
 
-var GitbugCache = cache.NewMultiRepoCache()
-var GitbugIds = make(map[string]*cache.IdentityCache, 0)
+var GitbugCache = bugcache.NewMultiRepoCache()
+var GitbugIds = make(map[string]*bugcache.IdentityCache, 0)
 
 // Test using git-bug API
 
@@ -21,28 +26,17 @@ func SetupGitbugCache(this js.Value, args []js.Value) (interface{}, error) {
 		println("repo_gitbug.go: GitbugCache was not created")
 	}
 
-	primaryRepo, err := createTestGitbugRepoCache("primary-repo")
+	entryPrimary, err := NewGitbugRepoCache("primary-repo")
 	if err != nil {
 		return nil, err
 	}
+	entryAnother, err := NewGitbugRepoCache("another-repo")
 
-	anotherRepo, err := createTestGitbugRepoCache("another-repo")
-	rene, _ := createTestGitbugIdentity(primaryRepo, "René Descartes", "rene@descartes.fr")
-	mark, _ := createTestGitbugIdentity(anotherRepo, "Mark Hughes", "gitbug@markhughes.com")
+	rene, _ := NewTestGitbugIdentity(entryPrimary, "René Descartes", "rene@descartes.fr")
+	mark, _ := NewTestGitbugIdentity(entryPrimary, "Mark Hughes", "gitbug@markhughes.com")
+	AddSampleBugs(entryPrimary, mark)
 
-	err = primaryRepo.SetUserIdentity(rene)
-	if err != nil {
-		return nil, err
-	}
-
-	// Add a few bugs
-	_, _, err = primaryRepo.NewBug("This bug is a triumph!", "Lots to say\nand\nno\ntime to\nsay\nit!")
-	_, _, err = primaryRepo.NewBug("One bug is good but two is better", "Nothing to see here though.")
-	bug3, _, err := primaryRepo.NewBug("Style the HTML/CSS to suit a git portal", "The app runs and uses default Svelte HTML/CSS styling, but initially it should mirror github to highlight its aim to replicate key features of a git portal like github. Later it can differentiate itself, but initially I think it will help to demonstrate the purpose of a proof-of-concept, what is working at any stage and where work is still needed.\n	As a starting point, we might look at using parts of the git-bug web UI, or build something from scratch for this Svelte framework.")
-
-	bug3.AddComment("First comment on a bug!")
-
-	err = anotherRepo.SetUserIdentity(mark)
+	err = entryAnother.GitbugRC.SetUserIdentity(rene)
 	if err != nil {
 		return nil, err
 	}
@@ -50,31 +44,99 @@ func SetupGitbugCache(this js.Value, args []js.Value) (interface{}, error) {
 	// TODO add code to create test commits (probably as a separate function)
 
 	// Test list bugs from cache
-	listBugsForRepo("primary-repo")
+	GetBugsForRepo("primary-repo")
 
 	// Test open repo from filesystem and list bugs
 	GitbugCache.Close()
-	GitbugCache = cache.NewMultiRepoCache()
+	GitbugCache = bugcache.NewMultiRepoCache()
 
-	return listBugsForRepo("primary-repo")
+	return GetBugsForRepo("primary-repo")
 }
 
-func createTestGitbugRepoCache(path string) (*cache.RepoCache, error) {
-	repo, err := repository.InitFsGoGitRepo(path, Filesystem)
+// args []js.Value:
+// [0] - path to worktree where repository will be created
+func NewRepository(this js.Value, args []js.Value) (interface{}, error) {
+	println("NewRepository()...")
+	path := args[0].String()
+
+	worktreeFs, err := Filesystem.Chroot(path)
 	if err != nil {
 		return nil, err
 	}
 
+	dotGitFs, err := Filesystem.Chroot(filepath.Join(path, ".git"))
+	if err != nil {
+		return nil, err
+	}
+
+	storage := storagefs.NewStorage(dotGitFs, cache.NewObjectLRUDefault())
+	gogitRepo, err := gogit.Init(storage, worktreeFs)
+	if err != nil {
+		println("gogit.Init() failed: ", err.Error())
+		return nil, err
+	}
+	repoCache, err := OpenRepo(path)
+	if err != nil {
+		println("OpenRepo() failed: ", err.Error())
+		return nil, err
+	}
+
+	entry := Entry{
+		"none",
+		path,
+		"none",
+		gogitRepo,
+		repoCache,
+	}
+
+	bilbo, _ := NewTestGitbugIdentity(&entry, "Bilbo Baggins", "bilbo@the.vale")
+	AddSampleBugs(&entry, bilbo)
+	AllRepositories[path] = &entry
+
+	return nil, nil
+}
+
+func AddSampleBugs(entry *Entry, user *bugcache.IdentityCache) {
+	println("AddSampleBugs() to ", entry.Path)
+	repo := entry.GitbugRC
+	err := repo.SetUserIdentity(user)
+	if err != nil {
+		return
+	}
+
+	// Add a few bugs
+	_, _, err = repo.NewBug("This bug is a triumph!", "Lots to say\nand\nno\ntime to\nsay\nit!")
+	_, _, err = repo.NewBug("One bug is good but two is better", "Nothing to see here though.")
+	bug3, _, err := repo.NewBug("Style the HTML/CSS to suit a git portal", "The app runs and uses default Svelte HTML/CSS styling, but initially it should mirror github to highlight its aim to replicate key features of a git portal like github. Later it can differentiate itself, but initially I think it will help to demonstrate the purpose of a proof-of-concept, what is working at any stage and where work is still needed.\n	As a starting point, we might look at using parts of the git-bug web UI, or build something from scratch for this Svelte framework.")
+
+	bug3.AddComment("First comment on a bug!")
+
+}
+
+func NewGitbugRepoCache(path string) (*Entry, error) {
+	repo, err := repository.InitFsGoGitRepo(path, Filesystem)
+	if err != nil {
+		return nil, err
+	}
 	repoCache, err := GitbugCache.RegisterRepository(path, repo)
 	if err != nil {
 		return nil, err
 	}
 
-	return repoCache, nil
+	entry := Entry{
+		"none",
+		path,
+		"none",
+		repo.GetGitRepo(),
+		repoCache,
+	}
+	AllRepositories[path] = &entry
+
+	return &entry, nil
 }
 
-func createTestGitbugIdentity(rc *cache.RepoCache, name string, email string) (*cache.IdentityCache, error) {
-	newId, err := rc.NewIdentity(name, email)
+func NewTestGitbugIdentity(entry *Entry, name string, email string) (*bugcache.IdentityCache, error) {
+	newId, err := entry.GitbugRC.NewIdentity(name, email)
 	// TODO push id to other repos (first add as remotes to current repo)
 	if err != nil {
 		return nil, err
@@ -84,7 +146,7 @@ func createTestGitbugIdentity(rc *cache.RepoCache, name string, email string) (*
 	return newId, nil
 }
 
-func openRepo(path string) (*cache.RepoCache, error) {
+func OpenRepo(path string) (*bugcache.RepoCache, error) {
 	rc, _ := GitbugCache.ResolveRepo(path)
 	if rc != nil {
 		return rc, nil
@@ -103,18 +165,18 @@ func openRepo(path string) (*cache.RepoCache, error) {
 	return rc, nil
 }
 
-func listBugsForRepo(path string) (map[string]interface{}, error) {
-	return listBugsForRepoCache(openRepo(path))
+func GetBugsForRepo(path string) ([]interface{}, error) {
+	return listBugsForRepoCache(OpenRepo(path))
 }
 
-func listBugsForRepoCache(repoCache *cache.RepoCache, err error) (map[string]interface{}, error) {
+func listBugsForRepoCache(repoCache *bugcache.RepoCache, err error) ([]interface{}, error) {
 	println("listBugsForRepoCache()...")
 	if err != nil || repoCache == nil {
 		return nil, err
 	}
 
-	bugs := make(map[string]interface{}, 0)
-	for _, id := range repoCache.AllBugsIds() {
+	bugs := make([]interface{}, len(repoCache.AllBugsIds()))
+	for bugIndex, id := range repoCache.AllBugsIds() {
 		// println("bugId:", (string)(id))
 		bugCache, err := repoCache.ResolveBug(id)
 		if err != nil {
@@ -131,6 +193,7 @@ func listBugsForRepoCache(repoCache *cache.RepoCache, err error) (map[string]int
 			for i, l := range snap.Labels {
 				labels[i] = l.String()
 			}
+			bugs[bugIndex] = bug
 		}
 	}
 
